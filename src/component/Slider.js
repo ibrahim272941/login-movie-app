e7bdc7d18c83f5edda42d7d8a2670073de67a28b
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2022-01-01 23:05:09 +0100

    Auth added on to Slider

--- src/component/Slider.js
+++ src/component/Slider.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 const IMG_API = "https://image.tmdb.org/t/p/original";
 const Slider = ({ movie }) => {
+  const { currentUser } = useContext(AuthContext);
   let navigate = useNavigate();
   const handleClick = (img) => {
     const { id, poster_path, title, overview } = img;
-    navigate("/trailer", {
-      state: { id, poster_path, title, overview },
-    });
+    currentUser
+      ? navigate("/trailer", {
+          state: { id, poster_path, title, overview },
+        })
+      : alert("Please Login");
   };
   return (
     <Carousel className="carousel" fade autoPlay={true} interval={2000}>
